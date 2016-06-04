ddf3078da47ed03f81ab138eb320c4810df1bb47
Cyberrebell <stephan.beller@web.de>
2016-06-04 10:08:43 +0200

    composer update & title rename

--- module/Application/src/Module.php
+++ module/Application/src/Module.php
@@ -8,6 +8,6 @@ class Module
 
     public function getConfig()
     {
-        return include __DIR__ . '/../config/module.config.php';
+        return require __DIR__ . '/../config/module.config.php';
     }
 }
