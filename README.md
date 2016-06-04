dae2b3bfc4dc6d783f0cbd4eb5181decc50754ef
Cyberrebell <chainsaw75@web.de>
2016-06-04 08:51:22 +0200

    initialisation of the application

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # BricksExampleApplication
 Showcase project for bricks modules and their use
+
+# Externals used in this project
+
+Special thanks to all creators of the used components:
+
+* Zend Framework
+* Bootstrap
+* jQuery
---
+++ config/application.config.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    'modules' => [
+        'Zend\ServiceManager\Di',
+        'Zend\Session',
+        'Zend\Mvc\Plugin\Prg',
+        'Zend\Mvc\Plugin\Identity',
+        'Zend\Mvc\Plugin\FlashMessenger',
+        'Zend\Mvc\Plugin\FilePrg',
+        'Zend\Mvc\I18n',
+        'Zend\Mvc\Console',
+        'Zend\Log',
+        'Zend\Form',
+        'Zend\Cache',
+        'Zend\Router',
+        'Zend\Validator',
+        'Application',
+    ],
+    'module_listener_options' => [
+        'module_paths' => [
+            './module',
+            './vendor',
+        ],
+        'config_glob_paths' => [
+            'config/autoload/{{,*.}global,{,*.}local}.php',
+        ],
+        'config_cache_enabled' => false,
+        'config_cache_key' => 'config.cache',
+        'module_map_cache_enabled' => false,
+        'module_map_cache_key' => 'module.map.cache',
+        'cache_dir' => 'data/cache/',
+        'check_dependencies' => true,
+    ],
+];
